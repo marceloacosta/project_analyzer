1e62aff68d42da0225e9fcc2e3994d29196170ef
marceloacosta <acosta.cavalero@gmail.com>
2024-01-11 12:08:14 -0300

    x

--- app_stream.py
+++ app_stream.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from io import BytesIO
+
 import numpy as np
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
